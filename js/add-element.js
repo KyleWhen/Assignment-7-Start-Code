ad76597ad35d652831808877e09cef16e2e586b4
KyleWhen <barker.kyle17@gmail.com>
2020-11-04 13:09:50 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,28 +3,47 @@
  */
 
 // get all the hot class elements- change them to cool class
-var nlist = document.querySelectorAll("li.hot");
-for(var i=0; i<nlist.length; i++){
-  nlist[1].setAttribute("class","cool");
-}
+
+$(".hot").each(function() {
+    $(this).removeClass("hot");
+    $(this).addClass("cool");
+  });
 
 // traverse the elements
-
-
-// add a new element
+$("#one").next().next().text("milk");
 
 
 // add by clicking the plus sign
-document.getElementById("add").addEventListener("click", addElement);
+$('#add').click(addElement);
+
+//add a new element
 
 function addElement() {
   // add a new element
+  $("#todo").append("<li><input type='text'></li>");
+
+  $('input').blur(function(){
+    $(this).parent().addClass("cool");
+
+    var userinput = $(this).val();
+  $(this).parent().text(userinput);
+  $("li").click(changeStyle);
+  });
   
 }
 
 
 //  click the li element will change the changeStyle
 function changeStyle() {
+  if($(this).hasClass('complete')){
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }else{
+    //if the class is cool, remove it and add complete
+    //if the class is hot, remove it and add complete
+    //else, add complete
+    $(this).addClass('complete');
+  }
 
 }
 
