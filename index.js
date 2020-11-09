20edbeed81971cc9f7e84ebdb091723439ab4fb5
KyleWhen <barker.kyle17@gmail.com>
2020-11-09 16:42:56 -0500

    1 file modified

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAQ3-DuFMINqU412qUeua5WMZZPU1l1EuU",
+  authDomain: "grocerylist-bde77.firebaseapp.com",
+  databaseURL: "https://grocerylist-bde77.firebaseio.com",
+  projectId: "grocerylist-bde77",
+  storageBucket: "grocerylist-bde77.appspot.com",
+  messagingSenderId: "139163594125",
+  appId: "1:139163594125:web:36edde353ef2c80e4861c4"
 };
 
 firebase.initializeApp(firebaseConfig);
